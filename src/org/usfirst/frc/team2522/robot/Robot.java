1886ee59ddeaac57c43f33c51b7e69ad3b3244ac
blomqcon <connor.blomquists@gmail.com>
2017-02-18 16:13:58 -0800

    Added/organized buttons and added gyro

--- src/org/usfirst/frc/team2522/robot/Robot.java
+++ src/org/usfirst/frc/team2522/robot/Robot.java
@@ -1,30 +1,22 @@
 package org.usfirst.frc.team2522.robot;
 
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
 import com.ctre.CANTalon;
+import com.kauailabs.navx.frc.AHRS;
 
-import edu.wpi.cscore.AxisCamera;
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
 //import edu.wpi.cscore.MjpegServer;
 //import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.UsbCamera;
 
 //import com.ctre.CANTalon;
-
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -39,68 +31,85 @@ public class Robot extends IterativeRobot
 	VictorSP leftDrive2 = new VictorSP(1);
 	VictorSP rightDrive1 = new VictorSP(2);
 	VictorSP rightDrive2 = new VictorSP(3);
-	
-	CANTalon pickup = new CANTalon(5);
-	CANTalon feeder = new CANTalon(2);
+	Spark unjammer = new Spark(4);
+
 	CANTalon shooter1 = new CANTalon(0);
 	CANTalon shooter2 = new CANTalon(1);
-	
+	CANTalon feeder = new CANTalon(2);
 	CANTalon climber1 = new CANTalon(3);
 	CANTalon climber2 = new CANTalon(4);
-	
-	Joystick leftStick = new Joystick(0);
-	Joystick rightStick = new Joystick(1);
-	Joystick operatorStick = new Joystick(2);
+	CANTalon pickup = new CANTalon(5);
 	
 	Encoder leftDriveEncoder = new Encoder(new DigitalInput(2), new DigitalInput(3));
 	Encoder rightDriveEncoder = new Encoder(new DigitalInput(4), new DigitalInput(5));
-	
 	Encoder shooterEncoder = new Encoder(new DigitalInput(6), new DigitalInput(7));
 	
 	RobotDrive myDrive;
 	
 	DoubleSolenoid shifter = new DoubleSolenoid(0, 3, 4);
-	
 	DoubleSolenoid intake = new DoubleSolenoid(0, 2, 5);
 	DoubleSolenoid gearWall = new DoubleSolenoid(0, 1, 6);
 	DoubleSolenoid gearPushout = new DoubleSolenoid(0, 0, 7);
+	
 	DoubleSolenoid gearDrapes = new DoubleSolenoid(1, 3, 4);
 	DoubleSolenoid shooterHood = new DoubleSolenoid(1, 2, 5);
 		
 	AnalogInput sensor = new AnalogInput(0);
+	AHRS gyro;
 	
-	// Joysticks //
-	Joystick leftstick = new Joystick(0);
-	Joystick rightstick = new Joystick(1);
-	Joystick operatorstick = new Joystick(2);
+	Joystick leftStick = new Joystick(0);
+	Joystick rightStick = new Joystick(1);
+	Joystick operatorStick = new Joystick(2);
 	
-	Button shiftButton = new Button(rightstick, 1, Button.ButtonType.Toggle);
-	Button intakeButton = new Button(rightstick, 2, Button.ButtonType.Toggle);
-	Button gearWallButton = new Button(rightstick, 3, Button.ButtonType.Toggle);
-	Button gearPushoutButton = new Button(rightstick, 4, Button.ButtonType.Toggle);
-	Button gearDrapesButton = new Button(rightstick, 5, Button.ButtonType.Toggle);
-	Button shooterHoodButton = new Button(rightstick, 6, Button.ButtonType.Toggle);
+	Button shiftButton = new Button(rightStick, 1, Button.ButtonType.Toggle);
+	Button intakeButton = new Button(operatorStick, 1, Button.ButtonType.Toggle);
+	Button gearWallButton = new Button(operatorStick, 2, Button.ButtonType.Toggle);
+	Button gearPushoutButton = new Button(operatorStick, 3, Button.ButtonType.Toggle);
+	Button gearDrapesButton = new Button(operatorStick, 4, Button.ButtonType.Toggle);
+	Button shooterHoodButton = new Button(operatorStick, 5, Button.ButtonType.Toggle);
+	
+	Button shooterButton = new Button(operatorStick, 6, Button.ButtonType.Hold);
+	Button pickupButton = new Button(operatorStick, 7, Button.ButtonType.Hold);
+	Button climberButton = new Button(operatorStick, 8, Button.ButtonType.Hold);
+	Button feederButton = new Button(operatorStick, 9, Button.ButtonType.Hold);
+	Button unjammerButton = new Button(operatorStick, 10, Button.ButtonType.Hold);
 
 	//  (<Wheel Diameter in Inches> * <Pi>) / (<Pulses Per Rotation> * <Encoder Mount Gearing> <Third Stage Gearing>)  //
 //	public static double driveTranDistancePerPulse = ( * 3.1415) / ( * ( / ) * ( / ));
 	
 	public void robotInit()
 	{
+		 try {
+				/***********************************************************************
+				 * navX-MXP:
+				 * - Communication via RoboRIO MXP (SPI, I2C, TTL UART) and USB.            
+				 * - See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
+				 * 
+				 * navX-Micro:
+				 * - Communication via I2C (RoboRIO MXP or Onboard) and USB.
+				 * - See http://navx-micro.kauailabs.com/guidance/selecting-an-interface.
+				 * 
+				 * Multiple navX-model devices on a single robot are supported.
+				 ************************************************************************/
+	            gyro = new AHRS(SPI.Port.kMXP);
+	        } catch (RuntimeException ex ) {
+	        }
+		 gyro.reset();
 //      frontLeftDriveEncoder.setDistancePerPulse(driveTranDistancePerPulse);
 //      frontLeftDriveEncoder.reset();
 //		frontRightDriveEncoder.setDistancePerPulse(driveTranDistancePerPulse);
 //		frontRightDriveEncoder.reset();
+//    	shooterEncoder.setDistancePerPulse(shooterDistancePerPulse);
+//    	shooterEncoder.reset();
 		
     	myDrive = new RobotDrive(leftDrive1, leftDrive2, rightDrive1, rightDrive2);
+    	
     	shifter.set(DoubleSolenoid.Value.kForward);
     	intake.set(DoubleSolenoid.Value.kForward);
     	gearWall.set(DoubleSolenoid.Value.kForward);
     	gearPushout.set(DoubleSolenoid.Value.kForward);
     	gearDrapes.set(DoubleSolenoid.Value.kForward);
     	shooterHood.set(DoubleSolenoid.Value.kForward);
-
-//    	shooterEncoder.setDistancePerPulse(shooterDistancePerPulse);
-//    	shooterEncoder.reset();
     	
     	visionThread = new Thread(() -> 
     	{
@@ -140,7 +149,6 @@ public class Robot extends IterativeRobot
 	boolean gearPushin = false;
 	boolean gearDraped = false;
 	boolean shooterHoodUp = false;
-	boolean LEDOn = false;
 	
 	public void teleopPeriodic()
 	{
@@ -230,7 +238,7 @@ public class Robot extends IterativeRobot
 			}
 		}
 
-		if (leftStick.getRawButton(2))
+		if (shooterButton.isPressed())
 		{
 			shooter1.set(1.0);
 			shooter2.set(1.0);
@@ -241,7 +249,7 @@ public class Robot extends IterativeRobot
 			shooter2.set(0.0);
 		}
 		
-		if (leftStick.getRawButton(3))
+		if (pickupButton.isPressed())
 		{
 			pickup.set(1.0);
 		}
@@ -249,7 +257,7 @@ public class Robot extends IterativeRobot
 			pickup.set(0.0);
 		}
 		
-		if (leftStick.getRawButton(4))
+		if (climberButton.isPressed())
 		{
 			climber1.set(1.0);
 			climber2.set(1.0);
@@ -260,7 +268,7 @@ public class Robot extends IterativeRobot
 			climber2.set(0.0);
 		}
 		
-		if (leftStick.getRawButton(5))
+		if (feederButton.isPressed())
 		{
 			feeder.set(1.0);
 		}
@@ -269,10 +277,23 @@ public class Robot extends IterativeRobot
 			feeder.set(0.0);
 		}
 		
-		
-		SmartDashboard.putNumber("photoelectricSensor", sensor.getVoltage());
+		if (unjammerButton.isPressed())
+		{
+			unjammer.set(1.0);
+		}
+		else
+		{
+			unjammer.set(0.0);
+		}
 		
 		ButtonFeeder.INSTANCE.feed();
+		
+		writeDashboard();
 	}
 	
+	public void writeDashboard()
+	{
+		SmartDashboard.putNumber("photoelectricSensor", sensor.getVoltage());
+		SmartDashboard.putNumber("gyro", gyro.getAngle());
+	}
 }
